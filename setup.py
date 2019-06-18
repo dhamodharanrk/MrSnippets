87ca623bcb765ca3c3f4cb69c5f89b0ae6c5337e
Dhamodharan <dhamodharanrk@users.noreply.github.com>
2019-06-18 15:04:54 +0530

    Update for 1.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ URL = "https://github.com/dhamodharanrk/MrSnippets"
 EMAIL = "dhamodharanrk@gmail.com"
 AUTHOR = "Dhamodharan Karuppuswamy"
 
-REQUIRED = ["beautifulsoup4>=4.3.3","PyMySQL>=0.9.3","pymongo>=3.8.0","user_agent>=0.1.9","ftfy>=5.5.1","tldextract>=2.2.1","bleach>=3.1.0","python-csv>=0.0.11","requests>=2.18.4","html5lib>=1.0b10","pytest-shutil>=1.6.0","Pillow>=2.2.1","pyodbc>=4.0.26"]
+REQUIRED = ["beautifulsoup4>=4.3.3","PyMySQL>=0.9.3","pymongo>=3.8.0","user_agent>=0.1.9","ftfy>=5.5.1","tldextract>=2.2.1","bleach>=3.1.0","requests>=2.18.4","html5lib>=1.0b10","pytest-shutil>=1.6.0","Pillow>=2.2.1","fuzzywuzzy>=0.17.0"]
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
